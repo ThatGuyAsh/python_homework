1227f606fee817ada26dbc445d2baf95e564b569
Ashton Thompson <ashtonthomp@gmail.com>
2026-07-13 09:53:03 -0400

    Clean up Assignment 2 structure

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,4 +1,3 @@
-# Task 2
 import csv
 import os
 import custom_module
@@ -6,35 +5,13 @@ from datetime import datetime
 
 employees = None
 employee_id_column = None
+minutes1 = None
+minutes2 = None
+minutes_set = None
 minutes_list = None
 
 
-def read_employees():
-    global employees
-
-    employees = {
-        "fields": [],
-        "rows": []
-    }
-
-    with open("employees.csv", "r") as file:
-        reader = csv.reader(file)
-
-        employees["fields"] = next(reader)
-
-        for row in reader:
-            employees["rows"].append(row)
-
-    return employees
-
-
-# Task 3 
-import csv
-
-employees = None
-employee_id_column = None
-
-
+# Task 2 - Read a CSV File
 def read_employees():
     global employees
     global employee_id_column
@@ -44,42 +21,35 @@ def read_employees():
         "rows": []
     }
 
-    with open("employees.csv", "r") as file:
-        reader = csv.reader(file)
+    try:
+        with open("../csv/employees.csv", "r") as file:
+            reader = csv.reader(file)
 
-        employees["fields"] = next(reader)
+            employees["fields"] = next(reader)
 
-        for row in reader:
-            employees["rows"].append(row)
+            for row in reader:
+                employees["rows"].append(row)
+
+    except FileNotFoundError:
+        print("employees.csv not found")
 
     employee_id_column = column_index("employee_id")
 
     return employees
 
 
+# Task 3 - Find the Column Index
 def column_index(column_name):
     return employees["fields"].index(column_name)
 
 
-read_employees()
-
-
-# Task 4
-def column_index(column_name):
-    return employees["fields"].index(column_name)
-
-
+# Task 4 - Find the Employee First Name
 def first_name(row_number):
     first_name_column = column_index("first_name")
     return employees["rows"][row_number][first_name_column]
 
 
-# Task 5
-def first_name(row_number):
-    first_name_column = column_index("first_name")
-    return employees["rows"][row_number][first_name_column]
-
-
+# Task 5 - Find the Employee: a Function in a Function
 def employee_find(employee_id):
     def employee_match(row):
         return int(row[employee_id_column]) == employee_id
@@ -89,22 +59,14 @@ def employee_find(employee_id):
     return matches
 
 
-# Task 6
-def employee_find(employee_id):
-    def employee_match(row):
-        return int(row[employee_id_column]) == employee_id
-
-    matches = list(filter(employee_match, employees["rows"]))
-
-    return matches
-
-
+# Task 6 - Find the Employee with a Lambda
 def employee_find_2(employee_id):
-    matches = list(filter(lambda row: int(row[employee_id_column]) == employee_id, employees["rows"]))
+    matches = list(filter(lambda row: int(row[employee_id_column]) == employee_id,
+                          employees["rows"]))
     return matches
 
 
-# Task 7
+# Task 7 -  Sort the Rows by last_name Using a Lambda
 def sort_by_last_name():
     last_name_column = column_index("last_name")
 
@@ -113,7 +75,7 @@ def sort_by_last_name():
     return employees["rows"]
 
 
-# Task 8
+# Task 8 - Create a dict for an Employee
 def employee_dict(row):
     employee = {}
 
@@ -124,33 +86,28 @@ def employee_dict(row):
     return employee
 
 
-# Task 9
+# Task 9 -  A dict of dicts, for All Employees
 def all_employees_dict():
     all_employees = {}
 
     for row in employees["rows"]:
         employee_id = row[employee_id_column]
-
         all_employees[employee_id] = employee_dict(row)
 
     return all_employees
 
 
-# Task 10
+# Task 10 - Use the os Module
 def get_this_value():
     return os.getenv("THISVALUE")
 
 
-# Task 11
+# Task 11 - Creating Your Own Module
 def set_that_secret(new_secret):
     custom_module.set_secret(new_secret)
 
 
-# Task 12
-minutes1 = None
-minutes2 = None
-
-
+# Task 12 - Read minutes1.csv and minutes2.csv
 def read_csv_file(filename):
     data = {
         "fields": [],
@@ -169,19 +126,13 @@ def read_csv_file(filename):
 
 
 def read_minutes():
-    minutes1 = read_csv_file("minutes1.csv")
-    minutes2 = read_csv_file("minutes2.csv")
+    minutes1 = read_csv_file("../csv/minutes1.csv")
+    minutes2 = read_csv_file("../csv/minutes2.csv")
 
     return minutes1, minutes2
 
 
-minutes1, minutes2 = read_minutes()
-
-
-# Task 13
-minutes_set = None
-
-
+# Task 13 - Create minutes_set
 def create_minutes_set():
     set1 = set(minutes1["rows"])
     set2 = set(minutes2["rows"])
@@ -189,11 +140,7 @@ def create_minutes_set():
     return set1.union(set2)
 
 
-minutes1, minutes2 = read_minutes()
-minutes_set = create_minutes_set()
-
-
-# Task 14
+# Task 14 - Convert to datetime
 def create_minutes_list():
     minutes = list(minutes_set)
 
@@ -205,15 +152,7 @@ def create_minutes_list():
     return minutes
 
 
-minutes1, minutes2 = read_minutes()
-minutes_set = create_minutes_set()
-minutes_list = create_minutes_list()
-
-
-print(minutes_list)
-
-
-# Task 15
+# Task 15 - Write Out Sorted List
 def write_sorted_list():
     minutes_list.sort(key=lambda x: x[1])
 
@@ -222,7 +161,7 @@ def write_sorted_list():
         minutes_list
     ))
 
-    with open("./minutes.csv", "w", newline="") as file:
+    with open("../csv/minutes.csv", "w", newline="") as file:
         writer = csv.writer(file)
 
         writer.writerow(minutes1["fields"])
@@ -233,7 +172,11 @@ def write_sorted_list():
     return converted_list
 
 
+# Global data 
+read_employees()
+
 minutes1, minutes2 = read_minutes()
+
 minutes_set = create_minutes_set()
+
 minutes_list = create_minutes_list()
-write_sorted_list()
